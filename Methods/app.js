850d0b452155af4d43eb6cbba6fc3cbfb8c82c51
Brandon Dalton <dabigin@gmail.com>
2022-11-22 06:18:11 -0600

    Section 21 Lecture 217 The Mysterious Keyword 'THIS'

--- Methods/app.js
+++ Methods/app.js
@@ -18,3 +18,16 @@ const myMath = {
     return num ** 3;
   },
 };
+// Mysterious Keyword "THIS"
+//
+const cat = {
+  name: "Blue Steele",
+  color: "grey",
+  breed: "scottish fold",
+  meow() {
+    console.log("THIS IS", this);
+    console.log(`${this.name} says MEOWWW`);
+  },
+};
+
+const meow2 = cat.meow;
