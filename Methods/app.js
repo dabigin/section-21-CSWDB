ea9cc8d9a4d4d2c529a29ff18348a9f8ea435c85
Brandon Dalton <dabigin@gmail.com>
2022-11-22 05:51:23 -0600

    Section 21 Lecture 216 Defining Methods

---
+++ Methods/app.js
@@ -0,0 +1,20 @@
+// Regular way to write a method
+// const myMath = {
+//   PI: 3.14159,
+//   square: function (num) {
+//     return num * num;
+//   },
+//   cube: function (num) {
+//     return num ** 3;
+//   },
+// };
+// Shorthand way to write a method
+const myMath = {
+  PI: 3.14159,
+  square(num) {
+    return num * num;
+  },
+  cube(num) {
+    return num ** 3;
+  },
+};
